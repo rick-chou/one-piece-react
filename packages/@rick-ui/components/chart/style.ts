097876f618de9564c6511ca2f1003862302dcc40
rick zhou <944268618@qq.com>
2023-06-07 21:46:34 +0800

    📦 @rickzhou/react-ui #11

--- packages/@rick-ui/components/chart/style.ts
+++ packages/@rick-ui/components/chart/style.ts
@@ -1,9 +1,6 @@
 import { css } from '@emotion/react';
 
 export const ContainerStyles = css`
-  width: 100%;
-  height: 100%;
-
   &:focus {
     outline: 2px solid transparent;
     outline-offset: 2px;
