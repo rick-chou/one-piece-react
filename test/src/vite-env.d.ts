f5ccd8f6de2598c0b00fd6a443a895b7525dc41a
rick zhou <944268618@qq.com>
2023-06-11 14:32:23 +0800

    🌈 @rickzhou/flowchart

--- test/src/vite-env.d.ts
+++ test/src/vite-env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="vite/client" />
 
-import '@emotion/react/jsx-runtime';
-
-declare module '@emotion/react/jsx-runtime' {
-  namespace JSX {
-    type ElementType = React.JSX.ElementType;
-  }
-}
