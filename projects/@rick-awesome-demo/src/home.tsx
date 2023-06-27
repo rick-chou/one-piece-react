dc60882f5a16954eb415724d4dba82e4c7955f65
Rick.Zhou <944268618@qq.com>
2023-06-27 22:22:08 +0800

    temp

--- projects/@rick-awesome-demo/src/home.tsx
+++ projects/@rick-awesome-demo/src/home.tsx
@@ -11,7 +11,7 @@ import '@/router';
 const SideBar = () => (
   <div css={sidebarStyle}>
     <a css={logoStyle} href="#">
-      @RickZhou
+      @ONE-PIECE
     </a>
     <div className="side-wrapper">
       <div className="side-title">MENU</div>
@@ -119,7 +119,7 @@ const Header = () => (
         src="https://images.unsplash.com/photo-1587918842454-870dbd18261a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=943&q=80"
         alt=""
       />
-      <div className="user-name">Thomas</div>
+      <div className="user-name">Rick.Zhou</div>
       <svg viewBox="0 0 492 492" fill="currentColor">
         <path d="M484.13 124.99l-16.11-16.23a26.72 26.72 0 00-19.04-7.86c-7.2 0-13.96 2.79-19.03 7.86L246.1 292.6 62.06 108.55c-5.07-5.06-11.82-7.85-19.03-7.85s-13.97 2.79-19.04 7.85L7.87 124.68a26.94 26.94 0 000 38.06l219.14 219.93c5.06 5.06 11.81 8.63 19.08 8.63h.09c7.2 0 13.96-3.57 19.02-8.63l218.93-219.33A27.18 27.18 0 00492 144.1c0-7.2-2.8-14.06-7.87-19.12z"></path>
       </svg>
@@ -165,7 +165,7 @@ const Content = () => (
             />
           </div>
           <div className="author-detail">
-            <div className="author-name">Thomas Hope</div>
+            <div className="author-name">Rick.Zhou Hope</div>
             <div className="author-info">
               53K views <span className="seperate"></span>2 weeks ago
             </div>
@@ -540,7 +540,7 @@ const Content = () => (
                 />
               </div>
               <div className="msg-wrapper">
-                <div className="msg__name video-p-name"> Thomas Hope</div>
+                <div className="msg__name video-p-name"> Rick.Zhou Hope</div>
                 <div className="msg__content video-p-sub">
                   {' '}
                   recusandae doloremque aperiam alias molestias
@@ -655,10 +655,10 @@ const Home = () => {
 
       <div className="wrapper">
         <Header />
-        {/* <Content /> */}
-        <div className="main-container">
+        <Content />
+        {/* <div className="main-container">
           <Outlet />
-        </div>
+        </div> */}
       </div>
     </div>
   );
