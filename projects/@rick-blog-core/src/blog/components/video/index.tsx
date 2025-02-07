574ddf301fd6a504716b2eb0eecd6fb4abb65e3d
Rick.Zhou <944268618@qq.com>
2025-02-07 19:47:35 +0800

    remove blog-core project

--- projects/@rick-blog-core/src/blog/components/video/index.tsx
+++
@@ -1,3 +0,0 @@
-/**
- * <iframe src="//player.bilibili.com/player.html?aid=403910513&bvid=BV1oV411L7xb&cid=1211091811&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
- */
--- projects/@rick-blog-core/src/core/hooks/useStore.ts
+++
@@ -1,6 +0,0 @@
-import type { AppDispatch, RootState } from '@/core/store';
-import type { TypedUseSelectorHook } from 'react-redux';
-import { useDispatch, useSelector } from 'react-redux';
-
-export const useRickDispatch: () => AppDispatch = useDispatch;
-export const useRickSelector: TypedUseSelectorHook<RootState> = useSelector;
